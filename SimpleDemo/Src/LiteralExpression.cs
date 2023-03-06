e1e04d38aff729a21b870b9b326a93216c504ce0
Organ1sm <ywwithpeople@gmail.com>
2023-03-06 16:57:50 +0800

    Update some detail

---
+++ SimpleDemo/Src/LiteralExpression.cs
@@ -0,0 +1,17 @@
+﻿namespace HyperLang.SimpleDemo;
+
+public sealed class LiteralExpression : Expression
+{
+    public override SyntaxKind Kind         => SyntaxKind.NumberExpression;
+    public          Token      LiteralToken { get; }
+
+    public LiteralExpression(Token literalToken)
+    {
+        LiteralToken = literalToken;
+    }
+
+    public override IEnumerable<Node> GetChildren()
+    {
+        yield return LiteralToken;
+    }
+}
