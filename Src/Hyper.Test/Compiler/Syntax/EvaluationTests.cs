669d5f40ab0b10515ae2e86e7389f49fdff61654
Organ1sm <ywwithpeople@gmail.com>
2023-03-13 23:55:19 +0800

    [Test]: Add evaluation test and fix evaluate != binary-expression.

---
+++ Src/Hyper.Test/Compiler/Syntax/EvaluationTests.cs
@@ -0,0 +1,42 @@
+﻿using Hyper.Compiler.Parser;
+using Hyper.Compiler.Symbol;
+using Hyper.Compiler.Syntax;
+using Xunit;
+
+namespace Hyper.Test.Compiler.Syntax;
+
+public class EvaluationTests
+{
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("+1", 1)]
+    [InlineData("-1", -1)]
+    [InlineData("14 + 12", 26)]
+    [InlineData("12 - 3", 9)]
+    [InlineData("4 * 2", 8)]
+    [InlineData("9 / 3", 3)]
+    [InlineData("(10)", 10)]
+    [InlineData("12 == 3", false)]
+    [InlineData("3 == 3", true)]
+    [InlineData("12 != 3", true)]
+    [InlineData("3 != 3", false)]
+    [InlineData("false == false", true)]
+    [InlineData("true == false", false)]
+    [InlineData("false != false", false)]
+    [InlineData("true != false", true)]
+    [InlineData("true", true)]
+    [InlineData("false", false)]
+    [InlineData("!true", false)]
+    [InlineData("!false", true)]
+    [InlineData("(a = 10) * a", 100)]
+    public void SyntaxFactGetText(string text, object expectedValue)
+    {
+        var ast         = AST.Parse(text);
+        var compilation = new Compilation(ast);
+        var variables   = new Dictionary<VariableSymbol, object>();
+        var result      = compilation.Evaluate(variables);
+
+        Assert.Empty(result.Diagnostics);
+        Assert.Equal(expectedValue, result.Value);
+    }
+}
