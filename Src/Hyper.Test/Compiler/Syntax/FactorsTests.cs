c3f4301fb8c67dc966f20fc5ecf73d4d9c04ef57
Organ1sm <ywwithpeople@gmail.com>
2023-03-13 11:28:09 +0800

    [Test]: Add Factors.GetText() and FactorsGetText Tests.

---
+++ Src/Hyper.Test/Compiler/Syntax/FactorsTests.cs
@@ -0,0 +1,30 @@
+﻿using Hyper.Compiler.Syntax;
+using Xunit;
+
+namespace Hyper.Test.Compiler.Syntax;
+
+public class FactorsTests
+{
+    [Theory]
+    [MemberData(nameof(GetSyntaxKindData))]
+    public void FactorsGetTextTest(SyntaxKind kind)
+    {
+        var text = Factors.GetText(kind);
+        if (text == null)
+            return;
+
+        var tokens = AST.ParseTokens(text);
+        var token  = Assert.Single(tokens);
+
+        Assert.Equal(kind, token.Kind);
+        Assert.Equal(text, token.Text);
+    }
+
+    public static IEnumerable<object[]> GetSyntaxKindData()
+    {
+        var kinds = (SyntaxKind[]) Enum.GetValues(typeof(SyntaxKind));
+
+        foreach (var kind in kinds)
+            yield return new object[] {kind};
+    }
+}
