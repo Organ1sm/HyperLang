df3b5ad363ba76fce0118a7216c89e0192c0555e
Organ1sm <ywwithpeople@gmail.com>
2023-03-14 23:58:42 +0800

    Fixed null reference for cases when the empty token inserted.

--- Src/Compiler/Parser/Token.cs
+++ Src/Compiler/Parser/Token.cs
@@ -18,6 +18,6 @@ namespace Hyper.Compiler.Parser
         public          int      Position { get; }
         public          string   Text     { get; }
         public          object?  Value    { get; }
-        public override TextSpan Span     => new(Position, Text.Length);
+        public override TextSpan Span     => new(Position, Text?.Length ?? 0);
     }
 }
