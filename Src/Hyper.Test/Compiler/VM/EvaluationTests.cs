68f28cc584490993008e99fc3d1b9b77f27325d6
Organ1sm <ywwithpeople@gmail.com>
2023-03-17 15:11:25 +0800

    Extract assert method

--- Src/Hyper.Test/Compiler/VM/EvaluationTests.cs
+++ Src/Hyper.Test/Compiler/VM/EvaluationTests.cs
@@ -29,7 +29,9 @@ public class EvaluationTests
     [InlineData("!true", false)]
     [InlineData("!false", true)]
     [InlineData("{ var a = 0 (a = 10) * a }", 100)]
-    public void EvaluatorComputesCorrectValues(string text, object expectedValue)
+    public void EvaluatorComputesCorrectValues(string text, object expectedValue) => AssertValue(text, expectedValue);
+
+    private static void AssertValue(string text, object expectedValue)
     {
         var ast         = AST.Parse(text);
         var compilation = new Compilation(ast);
