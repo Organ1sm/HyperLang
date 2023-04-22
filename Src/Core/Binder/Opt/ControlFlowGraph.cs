612b0a817d982e6baaef15112f417cc6d697e6e7
Organ1sm <ywwithpeople@gmail.com>
2023-04-22 11:06:15 +0800

    fix exception evaluation control paths for empty functions

--- Src/Core/Binder/Opt/ControlFlowGraph.cs
+++ Src/Core/Binder/Opt/ControlFlowGraph.cs
@@ -72,8 +72,8 @@ internal sealed class ControlFlowGraph
 
         foreach (var branch in graph.End.Incoming)
         {
-            var lastStatement = branch.From.Statements.Last();
-            if (lastStatement.Kind != BoundNodeKind.ReturnStatement)
+            var lastStatement = branch.From.Statements.LastOrDefault();
+            if (lastStatement == null || lastStatement.Kind == BoundNodeKind.ReturnStatement)
                 return false;
         }
 
