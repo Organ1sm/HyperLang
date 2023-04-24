562f7752dee89dd407fc2e91032ca58449e4e769
Organ1sm <ywwithpeople@gmail.com>
2023-04-24 16:06:33 +0800

    return the error code in HyperC

--- Src/Driver/HyperC/Program.cs
+++ Src/Driver/HyperC/Program.cs
@@ -8,12 +8,12 @@ namespace HyperC;
 internal static class Program
 
 {
-    private static void Main(string[] args)
+    private static int Main(string[] args)
     {
         if (args.Length == 0)
         {
             Console.Error.WriteLine("usage: mc <source-paths>");
-            return;
+            return 1;
         }
 
         var paths       = GetFilePaths(args);
@@ -25,7 +25,7 @@ internal static class Program
         {
             if (!File.Exists(path))
             {
-                Console.WriteLine($"error: file '{path}' doesn't exist");
+                Console.Error.WriteLine($"error: file '{path}' doesn't exist");
                 hasErrors = true;
                 continue;
             }
@@ -34,7 +34,7 @@ internal static class Program
             syntaxTrees.Add(syntaxTree);
         }
 
-        if (hasErrors) return;
+        if (hasErrors) return 1;
 
         var compilation = new Compilation(syntaxTrees.ToArray());
         var result      = compilation.Evaluate(new Dictionary<VariableSymbol, object>());
@@ -47,7 +47,10 @@ internal static class Program
         else
         {
             Console.Error.WriteDiagnostics(result.Diagnostics);
+            return 1;
         }
+
+        return 0;
     }
 
     private static IEnumerable<string> GetFilePaths(IEnumerable<string> paths)
